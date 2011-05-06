377b315b78d3ba83d6c8080afd68344de30e06b8
Niothiel <Niothiel@gmail.com>
2011-05-05 22:33:17 -0400

    Added functionality for adding courses to the database, as well as dumping out all of the courses.

--- dbadapter.py
+++ dbadapter.py
@@ -1,6 +1,4 @@
 class DBAdapter:
-	login = "SELECT * FROM Users WHERE email=$email AND pass=$passwd"
-
 	def __init__(self, db):
 		self.db = db
 
@@ -14,3 +12,21 @@ class DBAdapter:
 			return True
 		else:
 			return False
+
+	def addcourse(self, email, crn, term):
+		if not self._checkcourse(email, crn, term):
+			return False;
+
+		self.db.insert("Courses", email = email, crn = crn, term = term)
+		return True;
+	
+	def _checkcourse(self, email, crn, term):
+		courses = self.db.query("SELECT * FROM Courses WHERE email='%s' AND crn='%s' AND term='%s'" % (email, crn, term))
+		if len(courses) != 0:
+			return False
+		else:
+			return True
+
+	def getcourses(self, email):
+		courses = self.db.query("SELECT * FROM Courses WHERE email='%s'" % email)
+		return courses
